9e23c80f372329afa33157e3f1665d93f416f9b7
William <wcsimpso@ucsd.edu>
2019-09-26 14:50:44 -0700

    started writing functionality for buttons

--- app/src/main/java/com/example/integercalculator/MainActivity.java
+++ app/src/main/java/com/example/integercalculator/MainActivity.java
@@ -6,6 +6,16 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Constants for program
+    private static final int BUTTON_0_VAL = 0;
+
+    //String that holds what is currently displayed
+    private String displayContent = "0";
+
+    //Stores first operand
+    private int  operand1 = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
