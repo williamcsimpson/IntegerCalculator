480ee936a19461da5342a383f8364e5d04afed29
William <wcsimpso@ucsd.edu>
2019-09-26 14:44:47 -0700

    UI finished-ish

--- app/src/main/java/com/example/integercalculator/MainActivity.java
+++ app/src/main/java/com/example/integercalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.integercalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when user presses button0
+     *
+     * @param view used for something idk...
+     */
+    public void button0Pressed( View view ) {
+
     }
 }
